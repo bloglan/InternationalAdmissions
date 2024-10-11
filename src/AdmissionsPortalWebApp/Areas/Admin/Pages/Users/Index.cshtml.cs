4f6f50e27173d39cab9f773db6d6c0cf2d3be6c7
Jonny Yang <63300503@qq.com>
2024-10-11 14:31:04 +0800

    i18n and l10n.

--- src/AdmissionsPortalWebApp/Areas/Admin/Pages/Users/Index.cshtml.cs
+++ src/AdmissionsPortalWebApp/Areas/Admin/Pages/Users/Index.cshtml.cs
@@ -1,10 +1,20 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PersonIdentity;
 
 namespace AdmissionsPortalWebApp.Areas.Admin.Pages.Users;
 
-public class IndexModel : PageModel
+public class IndexModel(UserManager<ApplicationUser> userManager) : PageModel
 {
-    public void OnGet()
+
+    public IEnumerable<ApplicationUser> Users { get; set; } = [];
+    public void OnGet(string? q)
     {
+        IQueryable<ApplicationUser> users = userManager.Users;
+        if (!string.IsNullOrEmpty(q))
+        {
+            users = users.Where(u => u.UserName!.Contains(q) || u.Email!.Contains(q) || u.Name.Contains(q));
+        }
+        Users = users;
     }
 }
