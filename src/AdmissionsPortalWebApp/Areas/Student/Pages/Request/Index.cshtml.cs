fc95956417d1732fa278e11d1dc36d7173337293
Jonny Yang <63300503@qq.com>
2024-10-15 22:46:39 +0800

    修改首页，新建申请页。

---
+++ src/AdmissionsPortalWebApp/Areas/Student/Pages/Request/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdmissionsPortalWebApp.Areas.Student.Pages.Request
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet(int id)
+        {
+        }
+    }
+}
---
+++ src/AdmissionsPortalWebApp/Areas/Student/Pages/Request/Start.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdmissionsPortalWebApp.Areas.Student.Pages.Request
+{
+    public class StartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
